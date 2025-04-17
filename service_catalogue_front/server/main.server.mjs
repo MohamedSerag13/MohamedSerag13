11fb3e7c5e54972204ab5a46a6b54b89ed554879
Serage <serage@exab-kingabdulaziz.com>
2025-04-17 15:35:59 +0000

    nnn

---
+++ service_catalogue_front/server/main.server.mjs
@@ -0,0 +1,2 @@
+import './polyfills.server.mjs';
+import{a}from"./chunk-53TI4ZW2.mjs";import"./chunk-BYOD2JGJ.mjs";import"./chunk-EFCGTLPB.mjs";import"./chunk-DWIOQZ7J.mjs";import"./chunk-5XUXGTUW.mjs";export{a as default};
